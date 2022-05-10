0af8105adcf0e0cd62f014b1e3ed0c841f4ca72c
jacqueline ngabo <ngabo.jay@gmail.com>
2022-05-10 14:21:31 +0300

    add all changes

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY=77889900
-export MAIL_USERNAME=ngabogmail.com
+export MAIL_USERNAME=ngabo.jay@gmail.com
 export MAIL_PASSWORD=Mbuguack7
 export DATABASE_URL='postgresql+psycopg2://moringa:Mbuguack@localhost/pitches'
 python3.8 manage.py server
