a1e5a452eac20c426522d6bc9d31783355f25abc
jacqueline ngabo <ngabo.jay@gmail.com>
2022-05-12 00:05:52 +0300

    add all changes

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY=jayngabo
 export MAIL_USERNAME=ngabo.jay@gmail.com
 export MAIL_PASSWORD=jayngabo
-export DATABASE_URL=''
+export DATABASE_URL='postgresql+psycopg2://geejay:geejay@localhost/geejay'
 python3.8 manage.py server
 
